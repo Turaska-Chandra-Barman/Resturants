54956b9a717f2e5176ce0aa512fc08fbe63b2fca
Turaska-Chandra-Barman <barmanjatin372@gmail.com>
2024-12-17 21:32:43 +0600

    discover section complete

--- src/components/ui/ExpolorSection/ExploreSection.jsx
+++ src/components/ui/ExpolorSection/ExploreSection.jsx
@@ -7,6 +7,7 @@ const ExploreSection = () => {
         <div className='px-[182px]'>
             <div>
                 <h1 className='font-lexend font-extrabold text-[26px]'>MyFeedback for Business has resources to help you plan, start, grow, and advertise your small business</h1>
+                
             </div>
         </div>
     </Container>
